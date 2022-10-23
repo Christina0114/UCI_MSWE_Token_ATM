9e683acf2dae2918b87def2080f1b07ae540924e
lordchao <lycheechao@gmail.com>
2022-10-23 11:31:56 -0700

    fix pom

--- src/main/java/com/capstone/tokenatm/TokenAtmApplication.java
+++ src/main/java/com/capstone/tokenatm/TokenAtmApplication.java
@@ -7,12 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 @SpringBootApplication
 public class TokenAtmApplication {
-
 
     public static void main(String[] args) throws IOException {
         URL url = new URL("https://canvas.instructure.com/api/v1/courses");
