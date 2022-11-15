0280f9716b3153c831bf021a2b67c546f809a99e
lordchao <lycheechao@gmail.com>
2022-11-15 12:17:04 -0800

    add code from main again

--- token-service/src/main/java/com/capstone/tokenatm/entity/TokenCountEntity.java
+++ token-service/src/main/java/com/capstone/tokenatm/entity/TokenCountEntity.java
@@ -1,5 +1,8 @@
 package com.capstone.tokenatm.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -12,6 +15,20 @@ public class TokenCountEntity {
 
     private Integer token_count;
 
+    private String user_name;
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    private String user_email;
+
+    private Date timestamp;
+
     public Integer getUser_id() {
         return user_id;
     }
@@ -20,8 +37,17 @@ public class TokenCountEntity {
         this.user_id = user_id;
     }
 
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
     public Integer getToken_count() {
         return token_count;
+    }
+
+
+    public void setTimestamp(Date current_time) {
+        this.timestamp = current_time;
     }
 
     public void setToken_count(Integer token_count) {
